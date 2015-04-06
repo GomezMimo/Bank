b0796c8c8d67a4abd89b125ed99bbc5437f581a3
Juan Gomez <juan.camilo.mimo@gmail.com>
2015-04-05 23:14:55 -0500

    Add exceptions

--- Banco.java
+++ Banco.java
@@ -2,15 +2,41 @@ package banco;
 import javax.swing.*;
 import java.util.Date;
 public class Banco{
+    public double leerValor(String mensaje){
+        while(true){
+            try{
+                return Double.parseDouble(JOptionPane.showInputDialog(mensaje));
+            }
+            catch(NumberFormatException e){
+            
+            }
+        }
+    }
     public Banco(){
         Date t = new Date();
         JOptionPane.showMessageDialog(null, "La fecha es: " + t);
-        long ced = Long.parseLong(JOptionPane.showInputDialog("De la cedula"));
+        long ced;
+        while(true){
+            try{
+                ced = Long.parseLong(JOptionPane.showInputDialog("De la cedula"));
+                break;
+            }
+            catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null, "Valor incorrecto");
+            }
+        }
         String nom = JOptionPane.showInputDialog("De el nombre");
         CuentaAhorros ref;
         if(JOptionPane.showConfirmDialog(null, "Abre cuenta con valor?") == 0){
-            double val = Double.parseDouble(JOptionPane.showInputDialog("Valor indicial de su cuenta"));
-            ref = new CuentaAhorros(ced, nom, val);
+            while(true){
+                try{
+                    double val = Double.parseDouble(JOptionPane.showInputDialog("Valor inicial de su cuenta"));
+                    ref = new CuentaAhorros(ced, nom, val);
+                }
+                catch(NumberFormatException e){
+                    JOptionPane.showMessageDialog(null, "Valor incorrecto");
+                }
+            }
         }else{
             ref = new CuentaAhorros(ced, nom);
         }        
@@ -20,10 +46,10 @@ public class Banco{
             String op[] = {"Consignar", "Retirar", "Consultar saldo", "Salir"};
             i= JOptionPane.showOptionDialog(null, "Opcion", "Banco", 0, JOptionPane.QUESTION_MESSAGE, null, op, op[0]);
             switch(i){
-                case 0: double val = Double.parseDouble(JOptionPane.showInputDialog("Valor a consignar"));
+                case 0: double val = leerValor("Valor a consignar");
                         ref.consignar(val);
                         break;
-                case 1:  val = Double.parseDouble(JOptionPane.showInputDialog("Valor a retirar"));                         
+                case 1:  val = leerValor("Valor a retirar");                        
                         if(ref.retirar(val)){
                             JOptionPane.showMessageDialog(null, "Retiro: " + val);
                         }
